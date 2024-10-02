056335aed86c4ca3d030f0ea308857acabfa0ddf
heatnovea <karsonwashereonce@gmail.com>
2024-10-02 07:52:06 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Redirects to the respective URLs when the buttons are clicked
+document.getElementById('gamex').addEventListener('click', () => {
+    window.location.href = 'https://sites.google.com/view/unblockedgamex/home';
+});
+
+document.getElementById('heatnovea').addEventListener('click', () => {
+    window.location.href = 'https://sites.google.com/view/heatnovea/home';
+});
